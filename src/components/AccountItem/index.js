4c2a9d98b100bc7b75168ee674d3be38dfd44086
Tuan <vuanhtuanthon2@gmail.com>
2022-06-07 21:14:54 +0700

    Logic header to Search box and view search

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,13 +2,14 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
+import Image from '../Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/270961679_10159870536636108_2642967668131478092_n.jpg?_nc_cat=108&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=Iy3uhSF5eOIAX82VGZV&_nc_oc=AQlGblFx3blhDpGLM3pLu0PssnIWKPpwQQ1aGOxt1jxw5uakgfJmv36W-fE1OfdkB1cepaX3s7Bg-kcLmPmwwWOy&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT8jfWn3RX1dv9fmImHJEX_v7nVetrBx0laTJL_u-K77bQ&oe=628AE457"
                 alt="Hoang"
