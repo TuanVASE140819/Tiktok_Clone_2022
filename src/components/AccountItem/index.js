1979039269997afe51e47fa354da35b590c0ed5a
Tuan <vuanhtuanthon2@gmail.com>
2022-06-07 22:18:16 +0700

    Logic header (+ api _acc)

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,27 +1,24 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
-import Image from '../Image';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/270961679_10159870536636108_2642967668131478092_n.jpg?_nc_cat=108&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=Iy3uhSF5eOIAX82VGZV&_nc_oc=AQlGblFx3blhDpGLM3pLu0PssnIWKPpwQQ1aGOxt1jxw5uakgfJmv36W-fE1OfdkB1cepaX3s7Bg-kcLmPmwwWOy&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT8jfWn3RX1dv9fmImHJEX_v7nVetrBx0laTJL_u-K77bQ&oe=628AE457"
-                alt="Hoang"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen The Hoang</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>giao.lang.office</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
